c52302b8a9cd8877695923729df89022bb5a8bdc
Caio Viotti <caio.viotti@gmail.com>
2015-09-19 04:18:08 -0300

    Add Recipes template

--- src/nativeflavors/theme/theme/scripts/app.js
+++ src/nativeflavors/theme/theme/scripts/app.js
@@ -1,1 +1,29 @@
 $(document).foundation();
+
+document.addEventListener('DOMContentLoaded', function () {
+	objectFit.polyfill({
+		selector: 'img',
+		fittype: 'cover'
+	});
+});
+
+$('.recipe').click(function () {
+    $(this).addClass('active');
+});
+
+$('.recipe__close').click(function (e) {
+    $(this).closest('.recipe.active').removeClass('active');
+    e.stopPropagation();
+});
+
+$(document).ready(function() {
+    $(function(){
+ 		$('#Container').mixItUp({
+			animation: {
+				duration: 400,
+				effects: 'fade stagger(30ms) translateZ(-200px)',
+				easing: 'cubic-bezier(.55,0,.1,1)'
+			}
+		});
+    });
+});
