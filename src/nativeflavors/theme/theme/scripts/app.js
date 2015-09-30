4a6eef147a2530ba98ac2215f577fea2bb592c56
Caio Viotti <caio.viotti@gmail.com>
2015-09-30 16:16:43 -0300

    Add faq and more

--- src/nativeflavors/theme/theme/scripts/app.js
+++ src/nativeflavors/theme/theme/scripts/app.js
@@ -16,14 +16,65 @@ $('.recipe__close').click(function (e) {
     e.stopPropagation();
 });
 
+$('.question__title').click(function () {
+    $(this).closest('.faq__question').toggleClass('active');
+    $(this).siblings('.question__answer').slideToggle();
+});
+
+$('.faq__category').click(function () {
+	var currentCategory = $(this).text();
+	$('.faq__title--category').text(currentCategory);
+});
+
+window.onresize = function() {
+	if (Foundation.utils.is_small_only()) {
+		$('.faq__bar').addClass('is-small');
+		$('.faq__bar').removeClass('active');
+		$('.faq__categories').hide();	
+	} else {
+		$('.faq__bar').removeClass('is-small');
+		$('.faq__bar').removeClass('active');
+		$('.faq__categories').show();	
+	}
+}
+
+$('.faq__title').click(function () {
+    $(this).closest('.faq__bar.is-small').toggleClass('active');
+    $(this).siblings('.faq__categories').slideToggle();
+});
+
+$('.faq__category').click(function () {
+	$(this).closest('.faq__bar.active .faq__categories').slideToggle();
+	$(this).closest('.faq__bar.active').removeClass('active');
+
+});
+
+
+
+ 
+
+
 $(document).ready(function() {
     $(function(){
- 		$('#Container').mixItUp({
+ 		$('#recipes-mixitup').mixItUp({
 			animation: {
 				duration: 400,
 				effects: 'fade stagger(30ms) translateZ(-200px)',
 				easing: 'cubic-bezier(.55,0,.1,1)'
 			}
 		});
+
+ 		$('#faq-mixitup').mixItUp({
+			load: {
+			filter: 'none'
+			},
+			animation: {
+			duration: 700,
+			effects: 'translateX(-180%) stagger(80ms)',
+			easing: 'cubic-bezier(.55,0,.1,1)'
+			}
+ 		});
+
+
     });
 });
