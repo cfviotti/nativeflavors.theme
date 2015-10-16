6e9e21dea52dddbfa9a75018d9b04bb38ab11bd7
Caio Viotti <caio.viotti@gmail.com>
2015-10-16 04:08:24 -0300

    Adjust products styles

--- src/nativeflavors/theme/theme/scripts/app.js
+++ src/nativeflavors/theme/theme/scripts/app.js
@@ -41,14 +41,17 @@ $('.recipe').click(function () {
 $('.recipe__close').click(function (e) {
     $(this).closest('.recipe.active').removeClass('active');
     e.stopPropagation();
+    
 });
 
 $('.product__item').click(function () {
     $(this).addClass('active');
+    $('body').addClass('no-scroll');
 });
 
 $('.more-info__close').click(function (e) {
     $(this).closest('.product__item.active').removeClass('active');
+    $('body').removeClass('no-scroll');
     e.stopPropagation();
 });
 
@@ -91,10 +94,6 @@ $('.faq__category').click(function () {
 });
 
 
-
- 
-
-
 $(document).ready(function() {
 	$('body').bind('touchstart', function() {});
     $(function(){
@@ -120,4 +119,70 @@ $(document).ready(function() {
 
     });
 
+});
+
+// Simple Parallax
+
+
+(function($) {
+
+  $.fn.parallax = function(options) {
+
+    var windowHeight = $(window).height();
+
+    // Establish default settings
+    var settings = $.extend({
+      speed: 0.15,
+      direction: 'up'
+    }, options);
+
+    // Iterate over each object in collection
+    return this.each(function() {
+
+      // Save a reference to the element
+      var $this = $(this);
+
+      // Set up Scroll Handler
+      $(document).scroll(function() {
+
+        var scrollTop = $(window).scrollTop();
+        var scrollBottom = $(document).height() - $(window).scrollTop() - $(window).height();
+        var offset = $this.offset().top;
+        var offsetBottom = $(window).height() - $this.offset().top; - $this.height();
+        var height = $this.outerHeight();
+
+        // Check if above or below viewport
+        if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+          return;
+        }
+
+        
+
+        // Set direction of parallax
+        if (settings.direction == 'down') {
+        	var yBgPosition = Math.round((offset + scrollTop) * settings.speed);
+        } else if (settings.direction == 'up') {
+        	var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+        } else console.log('Direction not valid!')
+
+        // Apply the Y Background Position to Set the Parallax Effect
+        $this.css('background-position', 'center ' + yBgPosition + 'px');
+
+      });
+
+    });
+
+  }
+}(jQuery));
+$('.parallax-section').parallax({
+	speed : 0.5
+});
+
+$('.parallax-section-product-bg').parallax({
+	speed : 0.3
+});
+
+$('.parallax-section-product').parallax({
+	speed : 0.2 ,
+	direction : 'down'
 });
