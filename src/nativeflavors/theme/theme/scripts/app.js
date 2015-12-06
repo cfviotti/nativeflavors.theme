2523d1301d67aac5d4f07a721cc81337a1aa1491
cfviotti <caio@viotti.me>
2015-12-06 05:12:55 -0200

    Changes to slick styles

--- src/nativeflavors/theme/theme/scripts/app.js
+++ src/nativeflavors/theme/theme/scripts/app.js
@@ -112,7 +112,9 @@ $(document).ready(function() {
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
   });
 
 	$('body').bind('touchstart', function() {});
